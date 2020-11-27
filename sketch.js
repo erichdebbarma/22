4d629941ce2782442ccadf0050e0782d527e07aa
erichdebbarma <74238880+erichdebbarma@users.noreply.github.com>
2020-11-27 14:00:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+const Engine = Matter.Engine;
+ const World = Matter.World;
+ const Bodies = Matter.Bodies;
+ var engine,world,ground;
+var ball
+function setup() {
+  createCanvas(400,400);
+  engine = Engine.create();
+   world = engine.world;
+   var option ={ 
+     isStatic : true
+     }
+      ground = Bodies.rectangle(200,390,400,50,option);
+   World.add(world,ground);
+
+
+   var options ={ 
+   restitution:1.0
+    }
+
+    ball = Bodies.circle(200,100,20,options);
+     World.add(world,ball)
+}
+
+function draw() {
+  background(0);  
+  Engine.update(engine);
+   rectMode(CENTER) ;
+   rect(ground.position.x,ground.position.y,400,50);
+   ellipseMode(RADIUS);
+   ellipse(ball.position.x,ball.position.y,20,20);
+}
